c846ea630ecfb2fe54dfb96960e262b29d522133
Akshith suram <akshithsuram1@gmail.com>
2026-06-10 14:18:59 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# week1-python-basics
+Personal Introduction Program
+This program asks the user for:
+1.Name
+2.Age
+3.Hobby
+
+Then it displays a welcome message.
---
+++ personal_intro.py
@@ -0,0 +1,9 @@
+# Personal Introduction Program
+
+name = input("What is your name? ")
+age = input("How old are you? ")
+hobby = input("What is your hobby? ")
+
+print("\n🎉 Welcome", name, "🎉")
+print("You are", age, "years old.")
+print("You love", hobby + ".")
